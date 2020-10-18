d953560a0a00ea0e7beefa53b8915eaf767d1a59
macedo-developer <macedorenata.developer@gmail.com>
2020-10-17 22:32:32 -0300

    New pages and config routing

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import OrphanagesMap from "./pages/OrphanagesMap";
 import OrphanageDetails from "./pages/OrphanageDetails";
+import SelectMapPosition from "./pages/CreateOrphanage/SelectMapPosition";
+import OrphanageData from "./pages/CreateOrphanage/OrphanageData";
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -12,6 +14,8 @@ export default function Routes() {
       <Navigator screenOptions={{ headerShown: false }}>
         <Screen name="OrphanagesMap" component={OrphanagesMap} />
         <Screen name="OrphanageDetails" component={OrphanageDetails} />
+        <Screen name="SelectMapPosition" component={SelectMapPosition} />
+        <Screen name="OrphanageData" component={OrphanageData} />
       </Navigator>
     </NavigationContainer>
   );
